2c89d766aa4069a5e310051ed596652adc11999f
mahadhsn <hassam87@mcmaster.ca>
2025-08-27 21:03:15 -0400

    fix3

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,16 @@ import type { PostHogConfig } from "posthog-js";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import posthog from 'posthog-js';
 import { PostHogProvider } from "posthog-js/react";
+
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+    api_host: 'https://us.i.posthog.com',
+    defaults: '2025-05-24',
+    loaded: function (posthog) {
+        posthog.identify('[user unique id]')
+    },
+})
 
 const options: Partial<PostHogConfig> = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
